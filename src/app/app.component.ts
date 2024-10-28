bf570bb765be99efcc929f78f399dfa421588dcc
alimon777 <alimonna777@gmail.com>
2024-10-28 16:52:37 +0530

    demonstrates to send data between parent and child components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'parentchild';
+  EnterName = "John Doe";
+  EnterAge = "23";
+  EnterStatus = "Single";
+  EnterGender = "Male";
+  EnterHeight = "6.1";
+  parentData1 = "";
+  parentData2 = "";
+  parentData3 = "";
+  parentData4 = "";
+  parentData5 = "";
+  transferData() {
+    this.parentData1=this.EnterName;
+    this.parentData2=this.EnterAge;
+    this.parentData3=this.EnterStatus;
+    this.parentData4=this.EnterGender;
+    this.parentData5=this.EnterHeight;
+  }
+  value="";
+  sendData(value:string){
+    this.value=value;
+  }
 }
